1f4868620f282769d0369197c96ca0f682adcf6b
DwarfsLord <34789551+DwarfsLord@users.noreply.github.com>
2022-01-06 05:00:51 +0100

    thoughts about Join

--- include.php
+++ include.php
@@ -1,13 +1,13 @@
 <?php
-require 'identity.php';
+
 
 class Player{
     public $player_id;
     public $name;
     public $rating;
 }
-
-//$pdo = new PDO("mysql:host=$MYSQL_SERVER;dbname=$MYSQL_DB_NAME", $MYSQL_USER, $MYSQL_PASSWORD);
+// require 'identity.php';
+// $pdo = new PDO("mysql:host=$MYSQL_SERVER;dbname=$MYSQL_DB_NAME", $MYSQL_USER, $MYSQL_PASSWORD);
 
 $pdo = new PDO('mysql:host=localhost;dbname=elo', 'root', '');
 
