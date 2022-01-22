b0c4246ebd0252297638f2576ec7a4567a69e3ec
DwarfsLord <34789551+DwarfsLord@users.noreply.github.com>
2022-01-22 07:12:42 +0100

    index.php refactored for 1v1

--- calculator.php
+++ calculator.php
@@ -1,6 +1,8 @@
 <?php
 
-function join_elo($a, $b)
+declare(strict_types=1);
+
+function join_elo(int $a, int $b): int|float
 {
     return min(
         min($a, $b) + abs($a - $b) * 0.5 + (0.03 * abs($a - $b)) ** 2,
@@ -8,7 +10,7 @@ function join_elo($a, $b)
     );
 }
 
-function calculate_elo($a, $b, $a_won, &$a_elo_gained)
+function calculate_elo(int|float $a, int|float $b, bool $a_won, int|NULL &$a_elo_gained)
 {
     $MAX_ELO = 50;
     $CONST_A = 43; //43.4294
@@ -20,7 +22,7 @@ function calculate_elo($a, $b, $a_won, &$a_elo_gained)
         -1 * ceil($MAX_ELO * (1 - $b_win_percent));
 }
 
-function split_elo($a, $b, $elo_change, &$res_a, &$res_b)
+function split_elo(int $a, int $b, int $elo_change, int|NULL &$res_a, int|NULL &$res_b)
 {
     $BASE = 2;
     $DIVISOR = 70;
@@ -28,8 +30,8 @@ function split_elo($a, $b, $elo_change, &$res_a, &$res_b)
     $responsibility_a = 1 / (1 + $BASE ** (($a - $b) / $DIVISOR));
     $responsibility_a = ($elo_change < 0) ? 1 - $responsibility_a : $responsibility_a;
 
-    $res_a = round($responsibility_a * $elo_change * 2);
-    $res_b = round((1 - $responsibility_a) * $elo_change * 2);
+    $res_a = intval(round($responsibility_a * $elo_change * 2));
+    $res_b = intval(round((1 - $responsibility_a) * $elo_change * 2));
 
     //Guarantee change
     if ($res_a  == 0) {
@@ -51,4 +53,24 @@ function split_elo($a, $b, $elo_change, &$res_a, &$res_b)
             $res_b += $res_b <=> $res_a;
         }
     }
+}
+
+function add_free_elo(bool $game_won, int &$elo_same_type, int &$free_elo_same_type, int &$elo_other_type, int &$free_elo_other_type): bool
+{
+    $FREE_ELO_PER_GAME = 50;
+
+    if (($free_elo_same_type == 0 && $free_elo_other_type == 0) || ($game_won == false && $free_elo_same_type == 0)) {
+        return false;
+    }
+
+    $elo_same_type += min($free_elo_same_type, $FREE_ELO_PER_GAME);
+    $free_elo_same_type = max(0, $free_elo_same_type - $FREE_ELO_PER_GAME);
+
+    if ($game_won) {
+        $free_elo_gained = min($elo_same_type - $elo_other_type, $free_elo_other_type, $FREE_ELO_PER_GAME);
+        $free_elo_other_type -= $free_elo_gained;
+        $elo_other_type += $free_elo_gained;
+    }
+
+    return true;
 }
--- calculator_test.php
+++ calculator_test.php
@@ -1,3 +1,4 @@
+<pre>
 <?php
 require 'calculator.php';
 
@@ -17,33 +18,64 @@ function join_elo_test()
     echo join_elo(750.0, 1400.0) . "<br>";
 }
 
-function calculate_elo_test(){
+function calculate_elo_test()
+{
     $a = 1000;
     $b = 1000;
-    for ($i=0; $i < 10; $i++) { 
-        calculate_elo($a, $b, ($i+1)%2, $a_elo_gained);
+    // $a_elo_gained = 0;
+    for ($i = 0; $i < 10; $i++) {
+        calculate_elo($a, $b, ($i + 1) % 2, $a_elo_gained);
         $a += $a_elo_gained;
         $b -= $a_elo_gained;
         echo "$a <br>";
     }
-  
 }
 
 
-function split_elo_test(){
+function split_elo_test()
+{
     $a = 1045;
     $b = 1355;
     echo join_elo($a, $b) . "<br><table>";
     echo "<tr><td>$a</td><td>$b</td></tr>";
-    for ($i=0; $i < 10; $i++) { 
+    for ($i = 0; $i < 10; $i++) {
         split_elo($a, $b, 25, $a, $b);
         echo "<tr><td>$a</td><td>$b</td></tr>";
         split_elo($a, $b, 25, $a, $b);
         echo "<tr><td>$a</td><td>$b</td></tr>";
     }
-    echo "</table>".join_elo($a, $b) . "<br>";
+    echo "</table>" . join_elo($a, $b) . "<br>";
+}
+
+function add_free_elo_test()
+{
+    $elo1 = 750;
+    $f_elo1 = 250;
+    $elo2 = 750;
+    $f_elo2 = 250;
+
+    echo "<table>";
+    echo "<tr><td>$elo1</td><td>$f_elo1</td><td>$elo2</td><td>$f_elo2</td></tr>";
+
+    for ($i = 0; $i < 6; $i++) {
+
+        add_free_elo(true, $elo1, $f_elo1, $elo2, $f_elo2);
+        $elo1 += 25;
+        echo "<tr><td>$elo1</td><td>$f_elo1</td><td>$elo2</td><td>$f_elo2</td></tr>";
+
+        add_free_elo(false, $elo1, $f_elo1, $elo2, $f_elo2);
+        $elo1 -= 25;
+        echo "<tr><td>$elo1</td><td>$f_elo1</td><td>$elo2</td><td>$f_elo2</td></tr>";
+    }
+
+    // add_free_elo(true, $elo2, $f_elo2, $elo1,$f_elo1);
+    // $elo1 += 25;
+    // echo "<tr><td>$elo1</td><td>$f_elo1</td><td>$elo2</td><td>$f_elo2</td></tr>";
+
+    echo "</table>";
 }
 
 // split_elo_test();
-calculate_elo_test();
+// calculate_elo_test();
 // join_elo_test();
+// add_free_elo_test();
